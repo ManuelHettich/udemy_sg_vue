40e280723648ea2120d52885cd8210e20e3d7612
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2021-08-03 18:59:45 +0200

    Finish header

--- images/src/main.js
+++ images/src/main.js
@@ -1,14 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
-import AuthHandler from './components/AuthHandler';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      component: ImageList,
+    },
+    {
+      path: '/upload',
+      component: UploadForm,
+    },
     {
       path: '/oauth2/callback',
       component: AuthHandler,
