2175e1cdc7a5230080092a35c4de2c75bca8b7f6
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2021-08-03 09:34:27 +0200

    Add project video-browser

---
+++ identicon/identicon.vue
@@ -0,0 +1,30 @@
+<div id="app">
+  <h3>My Identicon Generator</h3>
+  <div>
+    Input: 
+    <input v-on:input="onInput" />
+  </div
+    
+  <div>
+    Output:
+    <div v-html="identicon" />
+  </div>
+</div>
+
+
+new Vue({
+  el: '#app',
+  data: {
+    textInput: '',
+  },
+  computed: {
+    identicon: function() {
+      return jdenticon.toSvg(this.textInput, 200);
+    }
+  },
+  methods: { // Use these functions to change data
+    onInput: function(event) {
+      this.textInput = event.target.value;
+    }
+  }
+});
