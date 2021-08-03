f9cd0f7fd7ecfe6830c24dc8b6182911dcb0f0e5
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2021-08-03 20:58:31 +0200

    Add Gallery feature

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    ImageList
+    <img v-for="image in allImages" :src="image.link" :key="image.id" />
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'ImageList',
+  computed: mapGetters(['allImages']),
+  methods: mapActions(['fetchImages']),
+  created() {
+    this.fetchImages();
+  },
 };
 </script>
